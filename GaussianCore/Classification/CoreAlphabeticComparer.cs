5f229eb578f78005e972db58576c9701f5468106
quanben <quanben@msn.com>
2015-09-23 08:45:10 +1000

    added tentative digi distance facilities.

---
+++ GaussianCore/Classification/CoreAlphabeticComparer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace GaussianCore.Classification
+{
+    public class CoreAlphabeticComparer : IComparer<ICore>
+    {
+        #region Properties
+
+        public static CoreAlphabeticComparer Instance { get; private set; } = new CoreAlphabeticComparer();
+
+        #endregion
+
+        #region Methods
+
+        #region IComparer<ICore> members
+
+        public int Compare(ICore x, ICore y)
+        {
+            for (var i = 0; i < x.CentersInput.Count; i++)
+            {
+                var xinput = x.CentersInput[i];
+                var yinput = y.CentersInput[i];
+                var c = xinput.CompareTo(yinput);
+                if (c != 0)
+                {
+                    return c;
+                }
+            }
+            for (var i = 0; i < x.CentersOutput.Count; i++)
+            {
+                var xoutput = x.CentersOutput[i];
+                var youtput = y.CentersOutput[i];
+                var c = xoutput.CompareTo(youtput);
+                if (c != 0)
+                {
+                    return c;
+                }
+            }
+            return 0;
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
---
+++ GaussianCore/Classification/DigiDistanceClassifier.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace GaussianCore.Classification
+{
+    public class DigiDistanceClassifier
+    {
+        #region Nested types
+
+        public class CodeCoreList
+        {
+            public string Code { get; set; }
+
+            public List<ICore> Cores { get; private set; } = new List<ICore>();
+
+            public void SortCores()
+            {
+                Cores.Sort(CoreAlphabeticComparer.Instance);
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        public double[] MatchThreshold { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public void Load(IList<CodeCoreList> codeCoreLists)
+        {
+
+        }
+
+        public static int Search (ICore core, IList<ICore> list, double[] matchThr)
+        {
+
+        }
+
+        #endregion
+    }
+}
